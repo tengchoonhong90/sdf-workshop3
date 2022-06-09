f3a35a86615e920e5521ac03ae851c03ade49a48
Teng Choon Hong <tengchoonhong_90@outlook.com>
2022-06-09 09:26:16 +0800

    add .gitignore

--- src/main/java/visa/shoppingCart/App.java
+++ src/main/java/visa/shoppingCart/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //print out first argument db name used tocreate the directory
+        System.out.println(args);
     }
 }
