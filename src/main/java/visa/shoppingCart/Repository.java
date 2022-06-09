8c6a4085065d36925284636a1f38f5de1b5854d9
Teng Choon Hong <tengchoonhong_90@outlook.com>
2022-06-09 13:55:43 +0800

    add repository.java

--- src/main/java/visa/shoppingCart/Repository.java
+++ src/main/java/visa/shoppingCart/Repository.java
@@ -1,5 +1,59 @@
 package visa.shoppingCart;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Repository {
-    
+    private File repository;
+
+    public Repository(String repo) {
+        this.repository = new File(repo);
+    }
+
+    public List<String> getShoppingCarts(){
+        List<String> carts = new LinkedList<>();
+        for(String n : repository.list())
+            carts.add(n.replace(".cart", ""));
+        System.out.println(carts);
+        return carts;
+    }
+
+    public void save(Cart cart){
+        String cartName = cart.getUsername() + ".cart";
+        String saveLocation = repository.getPath() + File.separator + cartName;
+        File saveFile = new File(saveLocation);
+        try{
+            if(!saveFile.exists())
+                saveFile.createNewFile();
+            OutputStream os = new FileOutputStream(saveLocation);
+            cart.save(os);
+            os.flush();
+            os.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Cart load(String username){
+        String cartName = username + ".cart";
+        Cart cart = new Cart(username);
+        for(File cartFile : repository.listFiles())
+            if(cartFile.getName().equals(cartName)) {
+                try {
+                    InputStream is = new FileInputStream(cartFile);
+                    cart.load(is);
+                    is.close();
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        return cart;
+    }
+
 }
