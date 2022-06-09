eba84b4d3083667474fac26fc1c1ce1a8303b2c8
Teng Choon Hong <tengchoonhong_90@outlook.com>
2022-06-09 09:20:18 +0800

    First commit

---
+++ src/main/java/visa/shoppingCart/App.java
@@ -0,0 +1,13 @@
+package visa.shoppingCart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
